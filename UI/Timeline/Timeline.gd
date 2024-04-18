be9f95c31e88477fb1b3909ecab409700060b31e
tploquin <timothee.ploquin@enseirb-matmeca.fr>
2024-04-18 20:54:12 +0200

    Timeline coloration

--- UI/Timeline/Timeline.gd
+++ UI/Timeline/Timeline.gd
@@ -21,6 +21,7 @@ var next_global_subdiv_input = 0
 
 const SPRITE_POS_ON_TIME = 0 # TODO Sync with beat
 const SPRITE_DIST_EACH_TIME = 100 # Depends on the png image
+const TOTAL_PATTERNS_VISIBLE = 20  # Num patterns to draw in advance
 
 const TEXTURE_PATTERN_BEAT = preload("res://UI/Timeline/pattern_beat.png")
 const TEXTURE_PATTERN_HALF_BEAT = preload("res://UI/Timeline/pattern_half_beat.png")
@@ -49,13 +50,13 @@ func _process(delta):
 	#	start_pattern()
 	
 	subdiv = get_subdiv_at_time()
-	set_sprite_pos(total_time)
+	set_sprite_pos()
 	check_input_missed()
 
 
 func reset_time(t=0.0):
 	total_time = t
-	set_sprite_pos(t)
+	set_sprite_pos()
 
 
 func await_pattern():
@@ -98,12 +99,14 @@ func get_input_error(input_received=1):
 	if awaiting and abs(error_on_beat) < max_error:
 		print("Timeline: CORRECT FIRST INPUT (error on beat :"+str(error_on_beat)+"/"+str(max_error)+")")
 		start_pattern(closest_div())
+		get_pattern_sprite_on_subdiv_id(closest_div()).modulate = Color(0.0, 0.8, 0.0)
 		print("Next subdiv awaited : "+str(next_pattern_subdiv_input))
 		return error_on_beat
 	elif abs(error) < max_error:
 		print("Timeline: CORRECT INPUT")
 		received_pattern[next_pattern_subdiv_input] = input_received
 		go_to_next_pattern_subdiv_input()
+		get_pattern_sprite_on_subdiv_id(closest_div()).modulate = Color(0.0, 0.8, 0.0)
 		print("Next subdiv awaited : "+str(next_pattern_subdiv_input))
 		return error
 	else:
@@ -133,6 +136,7 @@ func make_pattern_sprites():
 func make_all_pattern_sprites():
 	for c in $Pattern_sprites.get_children():
 		c.free()
+	"""old:
 	for r in range(-3, 12):
 		for i in range(len(pattern)):
 			if pattern[i]:
@@ -146,20 +150,58 @@ func make_all_pattern_sprites():
 					c.texture = TEXTURE_PATTERN_QUARTER_BEAT
 				c.position.x = (r * len(pattern) + i + offset) * SPRITE_DIST_EACH_TIME / MAX_SUBDIV
 				$Pattern_sprites.add_child(c)
+	"""
+	for r in range(TOTAL_PATTERNS_VISIBLE):
+		draw_pattern_sprites(r)
 
 
-func set_sprite_pos(t):
+func draw_pattern_sprites(r):
+	var c
+	for i in range(len(pattern)):
+		if pattern[i]:
+			c = Sprite2D.new()
+			# Global subdiv represented by this sprite
+			var equivalent_subdiv = r * len(pattern) + i + offset
+			c.set_name("Subdiv-"+str(i)+"-"+str(equivalent_subdiv))
+			if equivalent_subdiv % MAX_SUBDIV == 0:
+				c.texture = TEXTURE_PATTERN_BEAT
+			elif equivalent_subdiv % MAX_SUBDIV == MAX_SUBDIV / 2:
+				c.texture = TEXTURE_PATTERN_HALF_BEAT
+			else:
+				c.texture = TEXTURE_PATTERN_QUARTER_BEAT
+			c.position.x = equivalent_subdiv * SPRITE_DIST_EACH_TIME / MAX_SUBDIV
+			$Pattern_sprites.add_child(c)
+	c.set_name(c.name+"-") # Last (when deleted, generate new sprites for 1 more pattern)
+
+
+func set_sprite_pos(t=total_time):
 	var beat = bpm * t / 60.0
 	var p = fmod(beat, 1)
 	$Sprite2D.position.x = SPRITE_POS_ON_TIME - SPRITE_DIST_EACH_TIME * p
 	if awaiting:
-		# Redish on beat
+		# White, redish on beat
 		var gb = 1 - (2*p - 1) ** 4
 		$Pattern_sprites.modulate = Color(1, gb, gb)
 		$Pattern_sprites.position.x = SPRITE_POS_ON_TIME + SPRITE_DIST_EACH_TIME * 2
 	else:
+		"""old (just white) :
 		$Pattern_sprites.modulate = Color(1, 1, 1)
 		$Pattern_sprites.position.x = SPRITE_POS_ON_TIME + SPRITE_DIST_EACH_TIME * (2 - fmod(beat, len(pattern) / MAX_SUBDIV))
+		"""
+		$Pattern_sprites.modulate = Color(1, 1, 1) # No influence on children
+		# Light blue if "not yet done", else white
+		for sp in $Pattern_sprites.get_children():
+			var equivalent_subdiv = int(sp.name.split("-")[2]) # Name : Subdiv-1-1, Subdiv-3-42, Subdiv-0-864512
+			
+			if equivalent_subdiv >= next_global_subdiv_input: # Future
+				sp.modulate = Color(0.3, 0.3, 1.0)
+			
+			if sp.global_position.x < -10: # Out of screen -> deletion (optimisation)
+				if sp.name.ends_with("-"): # Last sprite of pattern -> redraw one more pattern
+					draw_pattern_sprites(TOTAL_PATTERNS_VISIBLE)
+				sp.queue_free()
+		
+		$Pattern_sprites.position.x = SPRITE_POS_ON_TIME + SPRITE_DIST_EACH_TIME * (2 - beat)
 
 
 func get_subdiv_at_time(time=total_time):
@@ -196,11 +238,20 @@ func get_pattern_at_subdiv(subdiv_ind=subdiv):
 func check_input_missed():
 	if get_subdiv_error(next_global_subdiv_input) > max_error:
 		print("missed (from Timeline.gd)")
+		get_pattern_sprite_on_subdiv_id(next_global_subdiv_input).modulate = Color(1, 0, 0)
 		go_to_next_pattern_subdiv_input()
 		print("now awaiting : "+str(next_pattern_subdiv_input)+" (global : "+str(next_global_subdiv_input))
 		input_missed.emit()
 		return
 	return
+
+
+func get_pattern_sprite_on_subdiv_id(subdiv):
+	var id
+	for sp in $Pattern_sprites.get_children():
+		id = int(sp.name.split("-")[2])
+		if id == subdiv:
+			return sp
 
 
 func get_next_non_zero_pattern(ind):
